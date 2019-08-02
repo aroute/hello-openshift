84ef57bcfb7edee94d1430867ed0dd6994756b0d
aroute <aroute@users.noreply.github.com>
2019-08-02 13:14:06 -0500

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>OCP Hello World</title>
+ </head>
+ <body>
+ <?php echo '<h2>Hello OpenShift</h2>'; ?> 
+ <?php $current_timestamp = time(); ?>
+ <?php echo '<h3>$current_timestamp;</h3>'; ?> 
+ </body>
+</html>
