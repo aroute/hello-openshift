ae9aabb857a7280b296a197cc26c2a371cc801ac
Arif <aroute@users.noreply.github.com>
2020-04-15 12:36:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>OCP Hello World</title>
  </head>
  <body>
- <?php echo '<h2 style="color:#125BA8";>Hello OpenShift!</h2>'; ?> 
+ <?php echo '<h2 style="color:#E25648";>Hello OpenShift!</h2>'; ?> 
  <?php $nextWeek = time() + (7 * 24 * 60 * 60);
    echo 'Today is:       '. date('Y-m-d') ."\n"; ?>
  </body>
